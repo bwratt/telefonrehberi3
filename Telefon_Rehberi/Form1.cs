2fc2477b82def06159b54d1f30a0d21c2a425b0f
Berat Kaplan <kaplanberat1306@gmail.com>
2023-06-08 16:04:57 +0300

    Ara ve Listele butonu aktif hale getirildi.

--- Telefon_Rehberi/Form1.cs
+++ Telefon_Rehberi/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Telefon_Rehberi
 {
@@ -15,6 +16,48 @@ namespace Telefon_Rehberi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        OleDbConnection bag = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;Data Source=telefonrehberi.mdb");
+        OleDbCommand komut = new OleDbCommand();
+
+        DataTable veritablosu = new DataTable();
+
+        public void listele()
+        {
+            try
+            {
+                veritablosu.Clear();
+                OleDbDataAdapter siringa = new OleDbDataAdapter("select * from Tablo1", bag);
+                siringa.Fill(veritablosu);
+                dataGridView1.DataSource = veritablosu;
+
+
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.ToString());
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            veritablosu.Clear();
+
+            OleDbDataAdapter siringa = new OleDbDataAdapter("select * from Tablo1 where Isim like '" + textBox1.Text + "%'", bag);
+            siringa.Fill(veritablosu);
+            dataGridView1.DataSource = veritablosu;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
         }
     }
 }
