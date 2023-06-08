1ac7bfd231dffdfbd17a1ea657f784320119b966
Berat Kaplan <kaplanberat1306@gmail.com>
2023-06-08 16:11:52 +0300

    Kişi Ekle butonu aktif hale getirildi.

--- Telefon_Rehberi/Form1.cs
+++ Telefon_Rehberi/Form1.cs
@@ -59,5 +59,38 @@ namespace Telefon_Rehberi
         {
             listele();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult onay;
+                onay = MessageBox.Show("Bu işlemi yapmak istediğinize emin misiniz? ", "DİKKAT", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (onay == DialogResult.Yes)
+                {
+                    string isim, soyisim, telefon, adres;
+                    
+
+                    isim = textBox2.Text;
+                    soyisim = textBox3.Text;
+                    telefon = textBox4.Text;
+                    adres = textBox5.Text;
+
+                    bag.Open();
+                    komut.Connection = bag;
+                    komut.CommandText = "insert into Tablo1 (Isim,Soyisim,Telefon,Adres) values ('" + isim + "','" + soyisim + "','" + telefon + "','" + adres + "')";
+                    komut.ExecuteNonQuery();
+                    MessageBox.Show("Kişi Kayıt Edildi!");
+                    bag.Close();
+
+                    listele();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Tekrar Deneyiniz.");
+            }
+        }
     }
 }
